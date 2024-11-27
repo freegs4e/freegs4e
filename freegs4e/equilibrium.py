a604f9c446fab90438b6b33b9fb45ed77938a48e
kpentland <kpentlan@N0617-MAC.local>
2024-11-27 15:38:21 +0000

    added capability to calculate striepoints easily

--- freegs4e/equilibrium.py
+++ freegs4e/equilibrium.py
@@ -26,6 +26,8 @@ import numpy as np
 from numpy import array, exp, linspace, meshgrid, pi
 from scipy import interpolate
 from scipy.integrate import romb  # Romberg integration
+import matplotlib.pyplot as plt
+import shapely as sh
 
 # Multigrid solver
 from . import critical, machine, multigrid, polygons
@@ -859,6 +861,58 @@ class Equilibrium:
         return 1.0 / (
             (1.0 / self.poloidalBeta2()) + (1.0 / self.toroidalBeta())
         )
+
+    def strikepoints(self):
+        """    
+        This function can be used to find the strikepoints of an equilibrium 
+        using the:
+            
+        - R and Z grids (2D)
+        - psi_total (2D) (i.e. the poloidal flux map)
+        - psi_boundary (single value)
+        - wall coordinates (N x 2)
+        
+    It should find the intersection between any points on the psi_boundary contour
+    of psi_total and the wall of the tokamak. 
+    """
+        
+        
+        # find contour object for psi_boundary
+        if self._profiles.flag_limiter:
+            cs = plt.contour(self.R, self.Z, self.psi(), levels=[self._profiles.psi_bndry])
+        else:
+            cs = plt.contour(self.R, self.Z, self.psi(), levels=[self._profiles.xpt[0][2]])
+        plt.close() # this isn't the most elegant but we don't need the plot itself
+        
+        # for each item in the contour object there's a list of points in (r,z) (i.e. a line)
+        psi_boundary_lines = []
+        for i, item in enumerate(cs.allsegs[0]):
+            psi_boundary_lines.append(item)
+        
+        
+        # use the shapely package to find where each psi_boundary_line intersects the wall (or not)
+        strikes = []
+        wall = np.array([self.tokamak.wall.R, self.tokamak.wall.Z]).T
+        curve1 = sh.LineString(wall)
+        for j, line in enumerate(psi_boundary_lines):
+            curve2 = sh.LineString(line)
+        
+            # find the intersection points
+            intersection = curve2.intersection(curve1)
+        
+            # extract intersection points
+            if intersection.geom_type == 'Point':
+                strikes.append(np.squeeze(np.array(intersection.xy).T))
+            elif intersection.geom_type == 'MultiPoint':
+                strikes.append(np.squeeze(np.array([geom.xy for geom in intersection.geoms])))
+    
+        # check how many strikepoints
+        if len(strikes) == 0:
+            out = None
+        else:
+            out = np.concatenate(strikes, axis=0)
+            
+        return out
 
 
 def refine(eq, nx=None, ny=None):
